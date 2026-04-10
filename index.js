c6cbdc97f50d770074aefa2aa3ddadd09e579065
Swathi <swathi.koundinyasa@gmail.com>
2026-04-10 11:14:35 +0530

    created index.js and loaded server.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Entry point for Render deployment
+// This file exists to maintain compatibility with deployment systems
+// that expect index.js as the main entry point
+
+require('./server.js');
